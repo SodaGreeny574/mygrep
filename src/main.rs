33f9de3d2c0255644594b350365b6e06806c70e2
SodaGreeny574 <2231027146@xaufe.edu.cn>
2024-06-03 00:23:05 +0800

    update

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+use mygrep::Config;
+
+fn main() {
+    let args:Vec<String>=env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        eprintln!("Promblem parsing arguments:{}",err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = mygrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
